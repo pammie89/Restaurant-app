a82b1f0305a0aac6d069a29e35428cb8cc097289
Pam Afaneh <pamela.afaneh@gmail.com>
2023-03-05 14:40:22 -0600

    corrected images

--- components/restaurantList.js
+++ components/restaurantList.js
@@ -59,7 +59,7 @@ function RestaurantList(props) {
             top={true}
             style={{ height: 200 }}
             src={
-              `http://localhost:1337` + res.image.url
+              `https://strapi-api-gwlj.onrender.com` + res.image.url
             }
           />
           <CardBody style={{backgroundColor: '#f0f5f6'}}>
